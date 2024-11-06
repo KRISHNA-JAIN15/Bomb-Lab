7fc5cfd04d4e7c8ffcabedc35c2180a5dded052b
Krishna Jain <151719244+KRISHNA-JAIN15@users.noreply.github.com>
2024-11-06 12:26:08 +0530

    Update asm_phase.asm

--- asm_phase.asm
+++ asm_phase.asm
@@ -1,18 +1,13 @@
-
-; asm_phase.asm
 global _start
 
 section .data
-Team_Number dq 101  ; Team number inserted dynamically
+Team_Number dq 101
 
 section .text
 _start:
-    ; Load team number and add 1313 to create the secret value
-    mov rax, [Team_Number]    ; Load team number
-    add rax, 1313             ; Add 1313 to get the secret value
+    mov rax, [Team_Number]
+    add rax, 1313
 
-    ; Exit syscall
-    mov rax, 60               ; syscall: exit
-    xor rdi, rdi              ; exit code 0
+    mov rax, 60
+    xor rdi, rdi
     syscall
-        
